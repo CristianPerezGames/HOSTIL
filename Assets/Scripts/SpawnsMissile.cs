e94b5b554b4505907c09efd56828bd71d3b52db4
Cristian Perez <crisselmai10@hotmail.es>
2020-02-02 02:00:39 -0300

    RevertCommits

--- Assets/Scripts/SpawnsMissile.cs
+++ Assets/Scripts/SpawnsMissile.cs
@@ -7,4 +7,77 @@ public class SpawnsMissile : MonoBehaviour
     
 
 
+<<<<<<< HEAD
+=======
+    public Coroutine rutineSpawn;
+
+    private void Start()
+    {
+        RoundManager.Instance.onChangeStateGame += StateGame;
+    }
+
+    public void SpawnMissile()
+    {
+        if(rutineSpawn == null)
+            rutineSpawn = StartCoroutine(RutineMissile());
+    }
+
+    void StopSpawnRutine()
+    {
+        if (rutineSpawn != null)
+        {
+            StopCoroutine(rutineSpawn);
+            rutineSpawn = null;
+        }
+    }
+
+    void StateGame(GameState _gameState)
+    {
+        switch (_gameState)
+        {
+            case GameState.Playing:
+                SpawnMissile();
+                break;
+            case GameState.EndGame:
+                StopSpawnRutine();
+                break;
+            case GameState.Pause:
+                StopSpawnRutine();
+                break;
+            case GameState.MiniGame:
+                StopSpawnRutine();
+                break;
+        }
+    }
+
+    IEnumerator RutineMissile()
+    {
+        while (true)
+        {
+            float timeInterval = Random.Range(0.3f, 2.0f);
+            yield return new WaitForSecondsRealtime(timeInterval);
+            var newMissile = Instantiate(missilePrefab, new Vector2(Random.Range(-6,6),7), Quaternion.identity);
+            newMissile.SetTargetReactor(GetLiveReactor());        
+        }
+    }
+
+    private GameObject GetLiveReactor(){
+        List<GameObject> liveReactors = new List<GameObject>();
+        if(!reactor1.GetComponent<ReactorEnergy>().IsBroken()){
+            liveReactors.Add(reactor1);
+        }
+        if(!reactor2.GetComponent<ReactorEnergy>().IsBroken()){
+            liveReactors.Add(reactor2);
+        }
+        if(!reactor3.GetComponent<ReactorEnergy>().IsBroken()){
+            liveReactors.Add(reactor3);
+        }
+
+        if (liveReactors.Count>0){
+            return liveReactors[Random.Range(0, liveReactors.Count)];
+        } else {
+            return null;
+        }        
+    }
+>>>>>>> parent of a18c9c3... Agrego el tema principal
 }
